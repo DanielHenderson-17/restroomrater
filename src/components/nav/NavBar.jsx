2ade7d43764bbfe5a00c2f88796a8aa07b455a70
Daniel Henderson <djhindu17@gmail.com>
2024-09-21 00:12:44 -0500

    navbar addition

---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,54 @@
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav>
+      <li className="navbar-item">
+        <Link to="/">
+          <img
+            className="navbar-image"
+            src="https://i.imgur.com/Mpo9h6p.png"
+            alt=""
+          />
+        </Link>
+      </li>
+
+      <ul className="navbar">
+        <li className="navbar-item">
+          <Link to="/locations">All Rates</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/my-ratings">My Ratings</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/patrons">Patrons</Link>
+        </li>
+        {localStorage.getItem("honey_user") ? (
+          <li className="navbar-item navbar-logout">
+            {" "}
+            {/* replace with user created*/}
+            <Link
+              className="navbar-link"
+              to=""
+              onClick={() => {
+                localStorage.removeItem("honey_user");
+                {
+                  /* replace with user created*/
+                }
+                navigate("/", { replace: true });
+              }}
+            >
+              Logout
+            </Link>
+          </li>
+        ) : (
+          ""
+        )}
+      </ul>
+    </nav>
+  );
+};
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,12 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
 import { LocationList } from "../components/locations/LocationList.jsx";
 import { Location } from "../components/locations/Location.jsx";
+import { NavBar } from "../components/nav/NavBar.jsx";
 
 export const ApplicationViews = () => {
   return (
     <Routes>
-      <Route path="/locations" element={<LocationList />} />
-      <Route path="/locations/:locationId" element={<Location />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="/locations" element={<LocationList />} />
+        <Route path="/locations/:locationId" element={<Location />} />
+      </Route>
     </Routes>
   );
 };
